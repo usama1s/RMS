4d9caa962ca659ac9c6fe312bc1d417fd145132e
usama1s <usama_1s@hotmail.com>
2023-06-01 02:00:05 +0500

    backend url added

--- src/config/AxiosBase.js
+++ src/config/AxiosBase.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000";
+// const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://codify.pk/ros";
 
 const api = axios.create({
   baseURL: BASE_URL,
